ee9b1d3cdb94569b83583d3c634114a966fabd2b
LadyEmNet <103427169+LadyEmNet@users.noreply.github.com>
2025-11-03 10:36:35 +0000

    Update Algoland week 7 badge and prize

--- assets/algoland.js
+++ assets/algoland.js
@@ -22,7 +22,7 @@
     { week: 4, assetId: '3257999517', distributors: [DEFAULT_DISTRIBUTOR], opensOn: '2025-10-13' },
     { week: 5, assetId: '3257999522', distributors: [DEFAULT_DISTRIBUTOR], opensOn: '2025-10-20' },
     { week: 6, assetId: '3257999512', distributors: [DEFAULT_DISTRIBUTOR], opensOn: '2025-10-27' },
-    { week: 7, assetId: '', distributors: [DEFAULT_DISTRIBUTOR], opensOn: '2025-11-03' },
+    { week: 7, assetId: '3300006143', distributors: [DEFAULT_DISTRIBUTOR], opensOn: '2025-11-03' },
     { week: 8, assetId: '', distributors: [DEFAULT_DISTRIBUTOR], opensOn: '2025-11-10' },
     { week: 9, assetId: '', distributors: [DEFAULT_DISTRIBUTOR], opensOn: '2025-11-17' },
     { week: 10, assetId: '', distributors: [DEFAULT_DISTRIBUTOR], opensOn: '2025-11-24' },
